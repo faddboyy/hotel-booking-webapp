08777b141a6bcd35f1ea98d486cb6b2e4a44f756
Faddboyy <fadillahmaulanaxyz02@gmail.com>
2025-11-22 11:34:15 +0700

    Fix Bug

--- auth.ts
+++ auth.ts
@@ -2,21 +2,33 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { prisma } from "@/lib/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import type { Adapter } from "next-auth/adapters";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [Google],
-  session: { strategy: "jwt" },
+  adapter: PrismaAdapter(prisma) as Adapter,
+
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+
+  session: {
+    strategy: "jwt",
+  },
+
   pages: {
     signIn: "/signin",
   },
+
   callbacks: {
     jwt({ token, user }) {
-      if (user) token.role = user.role;
+      if (user) token.role = (user as any).role;
       return token;
     },
     session({ session, token }) {
-      session.user.id = token.sub;
+      session.user.id = token.sub!;
       session.user.role = token.role;
       return session;
     },
