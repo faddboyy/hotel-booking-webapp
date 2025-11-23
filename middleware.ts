06b5f270d582492bd8825c1c4b6377e1f10240e7
Faddboyy <fadillahmaulanaxyz02@gmail.com>
2025-11-23 11:00:56 +0700

    Fix Bug

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
-import { auth } from "@/auth";
-
-const ProtectedRoutes = ["/reservation", "/checkout", "/admin"];
+import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
-  const session = await auth();
-  const isloggedIn = !!session?.user;
-  const role = session?.user.role;
+  const token = await getToken({
+    req: request,
+    secret: process.env.AUTH_SECRET,
+  });
+
   const pathname = request.nextUrl.pathname;
+  const isLoggedIn = !!token;
+  const role = token?.role;
+
+  const protectedRoutes = ["/reservation", "/checkout", "/admin"];
 
   // Protect routes
-  if (!isloggedIn && ProtectedRoutes.some((r) => pathname.startsWith(r))) {
+  if (!isLoggedIn && protectedRoutes.some((r) => pathname.startsWith(r))) {
     return NextResponse.redirect(new URL("/signin", request.url));
   }
 
   // Admin only
-  if (isloggedIn && role !== "admin" && pathname.startsWith("/admin")) {
+  if (isLoggedIn && role !== "admin" && pathname.startsWith("/admin")) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
   // Already logged in cannot access signin
-  if (isloggedIn && pathname.startsWith("/signin")) {
+  if (isLoggedIn && pathname.startsWith("/signin")) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
